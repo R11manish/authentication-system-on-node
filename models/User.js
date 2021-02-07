c035356c24f709ce38eef22a95d26a7110dfed81
Manish Rawat <github.jmtss@gmail.com>
2021-02-06 19:06:21 -0800

    json format ready

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator')
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true,'please enter a email'],
+    unique: true,
+    lowercase: true,
+    validate: [ isEmail, 'please enter a valid email']
+  },
+  password: {
+    type: String,
+    required: [true,'please enter an password'],
+    minlength: [6,'Minimum password length is 6 characters']
+  },
+});
+ 
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
